01778c3d80d6adf35c4a75141152843e41d2ee34
yene <yene@cocoadojo.com>
2015-08-15 13:39:03 +0200

    added Extension target

---
+++ Extension/ActionRequestHandler.h
@@ -0,0 +1,13 @@
+//
+//  ActionRequestHandler.h
+//  Extension
+//
+//  Created by Yannick Weiss on 15/08/15.
+//  Copyright © 2015 Yannick Weiss. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ActionRequestHandler : NSObject <NSExtensionRequestHandling>
+
+@end
---
+++ Extension/ActionRequestHandler.m
@@ -0,0 +1,26 @@
+//
+//  ActionRequestHandler.m
+//  Extension
+//
+//  Created by Yannick Weiss on 15/08/15.
+//  Copyright © 2015 Yannick Weiss. All rights reserved.
+//
+
+#import "ActionRequestHandler.h"
+
+@interface ActionRequestHandler ()
+
+@end
+
+@implementation ActionRequestHandler
+
+- (void)beginRequestWithExtensionContext:(NSExtensionContext *)context {
+    NSItemProvider *attachment = [[NSItemProvider alloc] initWithContentsOfURL:[[NSBundle mainBundle] URLForResource:@"blockerList" withExtension:@"json"]];
+    
+    NSExtensionItem *item = [[NSExtensionItem alloc] init];
+    item.attachments = @[attachment];
+    
+    [context completeRequestReturningItems:@[item] completionHandler:nil];
+}
+
+@end
